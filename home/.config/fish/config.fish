82d0f5705508511b5167978cdae745c3a3343433
Andrei Borisov <andrei@borisov.dev>
2022-06-20 00:29:02 +0400

    Only enable prompt and Homebrew/command-not-found in interactive mode

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -13,11 +13,6 @@ set --global --export MANPATH $HOMEBREW_PREFIX/share/man $MANPATH
 set --global --export INFOPATH $HOMEBREW_PREFIX/share/info $INFOPATH
 fish_add_path --global $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
 
-# Homebrew/command-not-found
-set --local homebrew_command_not_found_handler $HOMEBREW_REPOSITORY/Library/Taps/homebrew/homebrew-command-not-found/handler.fish
-if test -f $homebrew_command_not_found_handler
-  source $homebrew_command_not_found_handler
-end
 
 # pnpm
 set --global --export PNPM_HOME $HOME/Library/pnpm
@@ -26,5 +21,13 @@ fish_add_path --global $PNPM_HOME
 # Rust
 fish_add_path --global $HOME/.cargo/bin
 
-# Starship
-starship init fish | source
+if status is-interactive
+  # Homebrew/command-not-found
+  set --local homebrew_command_not_found_handler $HOMEBREW_REPOSITORY/Library/Taps/homebrew/homebrew-command-not-found/handler.fish
+  if test -f $homebrew_command_not_found_handler
+    source $homebrew_command_not_found_handler
+  end
+
+  # Starship
+  starship init fish | source
+end
